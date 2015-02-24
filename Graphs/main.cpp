1261bd3a4f19f279196a584a13aba56bf4fc129f
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-02-24 21:09:37 +0200

    Initial Commit

---
+++ Graphs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphs
+//
+//  Created by Viktoras Laukevičius on 24/02/15.
+//  Copyright (c) 2015 Viktoras Laukevicius. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
