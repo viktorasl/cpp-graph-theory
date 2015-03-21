6901f54d90fd2c0456e0f89eecac233f12a37e63
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-03-21 12:42:15 +0200

    Refactored graph unit test

--- GraphsTests/TestGraph.cpp
+++ GraphsTests/TestGraph.cpp
@@ -19,23 +19,32 @@ public:
     {
         Graph graph;
         
-        Vertex *s0 = new Vertex(VertexTypeSource, 1);
-        Vertex *s1 = new Vertex(VertexTypeSource, 2);
-        Vertex *d0 = new Vertex(VertexTypeDestination, 1);
-        Vertex *d1 = new Vertex(VertexTypeDestination, 2);
+        int sCount = 2;
+        int dCount = 2;
+        Vertex *s[sCount];
+        for (int i = 0; i < sCount; i++) {
+            s[i] = new Vertex(VertexTypeSource, i);
+            graph.sourceVertexes.push_back(s[i]);
+        }
+        Vertex *d[dCount];
+        for (int i = 0; i < sCount; i++) {
+            d[i] = new Vertex(VertexTypeDestination, i);
+            graph.destinationVertexes.push_back(d[i]);
+        }
         
-        graph.sourceVertexes.push_back(s0);
-        graph.sourceVertexes.push_back(s1);
-        graph.destinationVertexes.push_back(d0);
-        graph.destinationVertexes.push_back(d1);
-        
-        s0->connectToVertex(d0); d0->connectToVertex(s0);
-        s0->connectToVertex(d1); d1->connectToVertex(s0);
+        connectEachOther(s[0], d[0]);
+        connectEachOther(s[0], d[1]);
         
         cout << __func__ << ":test1: " << ((graph.isConnected() == false)? "passed" : "failed") << endl;
         
-        s1->connectToVertex(d0); d0->connectToVertex(s1);
+        connectEachOther(s[1], d[0]);
         
         cout << __func__ << ":test2: " << ((graph.isConnected() == true)? "passed" : "failed") << endl;
+    }
+private:
+    void connectEachOther(Vertex *v1, Vertex *v2)
+    {
+        v1->connectToVertex(v2);
+        v2->connectToVertex(v1);
     }
 };
